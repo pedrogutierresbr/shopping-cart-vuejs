0d91deb2172dddc4149e2ff308201d5b18bf0dff
Pedro Gutierres <pedro-gabriel1@hotmail.com>
2023-05-10 22:16:04 -0300

    created fetch method for the products and displayed in the window

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 const vm = new Vue({
 	el: "#app",
-	data: {},
+	data: {
+		products: [],
+	},
+	methods: {
+		async getProducts() {
+			const url = "./api/produtos.json";
+			const products = await fetch(url)
+				.then((response) => response.json())
+				.then((data) => (this.products = data));
+		},
+	},
+	created() {
+		this.getProducts();
+	},
 });
