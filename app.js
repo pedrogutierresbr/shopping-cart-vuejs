f003702982b9fc7c633f95e6ebfc1de19be87843
Pedro Gutierres <pedro-gabriel1@hotmail.com>
2023-05-12 11:59:49 -0300

    created closeProduct and openModal methods together with animations for modal

--- app.js
+++ app.js
@@ -22,6 +22,16 @@ const vm = new Vue({
 				.then((response) => response.json())
 				.then((data) => (this.produto = data));
 		},
+		abrirModal(id) {
+			this.fetchProduto(id);
+			window.scrollTo({
+				top: 0,
+				behavior: "smooth",
+			});
+		},
+		fecharModal({ target, currentTarget }) {
+			if (target === currentTarget) this.produto = false;
+		},
 	},
 	created() {
 		this.fetchProdutos();
